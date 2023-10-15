6ee4a98c77a0b5185e1cb6230655a8308199ae3b
mrtstg <russiankurwacsgo@gmail.com>
2023-10-15 18:23:53 +0300

    added server launch code

---
+++ app/Main.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Main
+  ( main
+  ) where
+
+import Database.Redis (ConnectInfo(..), PortID(..), connect, defaultConnectInfo)
+import Env (getIntFromEnv, getStringFromEnv)
+import Foundation
+import Handlers.Home (getHomeR)
+import Handlers.Post (getPostR)
+import Yesod.Core
+
+mkYesodDispatch "App" resourcesApp
+
+main :: IO ()
+main = do
+  postDepthLimit <- getIntFromEnv "DEPTH_LEVEL" 1
+  redisHost <- getStringFromEnv "REDIS_HOST" "localhost"
+  redisPort <- getIntFromEnv "REDIS_PORT" 6379
+  redisConnectionPool <-
+    connect
+      defaultConnectInfo {connectHost = redisHost, connectPort = PortNumber (read $ show redisPort)}
+  warp 3000 App {..}
