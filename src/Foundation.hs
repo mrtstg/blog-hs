3743e74eba1bcac2f3e476c8f3fc949a65bb4962
mrtstg <russiankurwacsgo@gmail.com>
2023-12-30 18:26:23 +0300

    added missing language extension

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -13,6 +13,7 @@
 {-# LANGUAGE TypeFamilies               #-}
 {-# LANGUAGE TypeOperators              #-}
 {-# LANGUAGE UndecidableInstances       #-}
+{-# LANGUAGE ViewPatterns               #-}
 
 module Foundation where
 
