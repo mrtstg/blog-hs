04a9cec4e7a6391466f56aacedf7e74a18020524
mrtstg <russiankurwacsgo@gmail.com>
2024-01-04 12:53:12 +0300

    added listing of latest posts and pages links

--- src/Handlers/Home.hs
+++ src/Handlers/Home.hs
@@ -1,16 +1,47 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE RecordWildCards   #-}
 {-# LANGUAGE TypeFamilies      #-}
 
 module Handlers.Home
   ( getHomeR
   ) where
 
+import           Crud             (isPostsAvailable, selectLatestPosts)
+import           Data.Text
+import           Database.Persist (Entity (..))
 import           Foundation
+import           System.FilePath  (dropExtensions)
+import           Text.Read
 import           Yesod.Core
 
+getPageValue :: Maybe Text -> Int
+getPageValue param = case param of
+  Nothing -> 1
+  (Just v) -> case (readMaybe (unpack v) :: Maybe Int) of
+    (Just v') -> v'
+    Nothing   -> 1
+
 getHomeR :: Handler Html
-getHomeR =
+getHomeR = do
+  App { .. } <- getYesod
+  pageParam <- lookupGetParam "page"
+  let pageValue = getPageValue pageParam
+  posts <- liftIO $ selectLatestPosts dbPath pageValue
+  postsAvailable <- liftIO $ isPostsAvailable dbPath pageValue
   defaultLayout $ do
-    setTitle "123"
-    [whamlet|<p> Hello!|]
+    setTitle "Latest posts"
+    [whamlet|
+$if Prelude.null posts
+    <h1> No posts!
+$else
+    <ul>
+        $forall post <- posts
+            $case post
+                $of Entity _ post'
+                    <li><a href="/post/#{(dropExtensions . postFile) post'}">#{postTitle post'}
+$if pageValue /= 1
+    <a href="/?page=#{pageValue - 1}">Previous page
+$if postsAvailable
+    <a href="/?page=#{pageValue + 1}">Next page
+|]
