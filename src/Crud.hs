c02ceaaea91cf17638de78776bb74e8103de0b2e
mrtstg <russiankurwacsgo@gmail.com>
2024-01-20 19:33:41 +0300

    refactored crud module, moved out function for initiating posts in module

--- src/Crud.hs
+++ src/Crud.hs
@@ -7,18 +7,24 @@ module Crud
   ( findPostByFilename,
   selectLatestPosts,
   isPostsAvailable,
-  selectAllPosts
+  selectAllPosts,
+  createPostPhoto,
+  initiatePosts
   ) where
 
+import           App.PostInfo            (PostInfo (..))
+import           App.Utils               (normaliseFilePath)
 import           Data.Text               (Text)
 import           Database.Persist
 import           Database.Persist.Sqlite
 import           Foundation
 
+type DbPath = Text
+
 pageSize :: Int
 pageSize = 10
 
-findPostByFilename :: Text -> String -> IO (Maybe (Entity Post))
+findPostByFilename :: DbPath -> String -> IO (Maybe (Entity Post))
 findPostByFilename dbPath fPath =
   runSqlite dbPath $ do
     posts <- selectList [PostFile ==. fPath] [LimitTo 1]
@@ -26,13 +32,32 @@ findPostByFilename dbPath fPath =
       []    -> return Nothing
       (p:_) -> return $ Just p
 
-selectLatestPosts :: Text -> Int -> IO [Entity Post]
+selectLatestPosts :: DbPath -> Int -> IO [Entity Post]
 selectLatestPosts dbPath pageNumber = runSqlite dbPath $ do selectList [] [Desc PostDate, OffsetBy (pageSize * max 0 (pageNumber - 1)), LimitTo 10]
 
-selectAllPosts :: Text -> IO [Entity Post]
+selectAllPosts :: DbPath -> IO [Entity Post]
 selectAllPosts dbPath = runSqlite dbPath $ do selectList [] []
 
-isPostsAvailable :: Text -> Int -> IO Bool
+isPostsAvailable :: DbPath -> Int -> IO Bool
 isPostsAvailable dbPath pageNumber = runSqlite dbPath $ do
     postsCount <- count ([] :: [Filter Post])
     if postsCount - (pageSize * max 1 pageNumber) > 0 then return True else return False
+
+createPostPhoto :: DbPath -> Entity Post -> String -> IO ()
+createPostPhoto dbPath (Entity pid _) url = do
+    runSqlite dbPath $ do
+        _ <- insert $ PostPhoto url pid
+        return ()
+
+type PostsAmount = Int
+initiatePosts :: DbPath -> [(FilePath, PostInfo)] -> IO PostsAmount
+initiatePosts dbPath = helper 0 where
+    helper :: Int -> [(FilePath, PostInfo)] -> IO PostsAmount
+    helper acc [] = return acc
+    helper acc ((f, PostInfo name' desc dt img):ps) = do
+        runSqlite dbPath $ do
+            postId <- insert $ Post (normaliseFilePath f) name' desc dt
+            case img of
+              Nothing     -> return ()
+              (Just img') -> mapM_ (\x -> insert $ PostPhoto x postId) img'
+        helper (acc + 1) ps
